3b9e31e64f510f1302ffcecf997c6207bd33109f
slutsk <54749775+slutsk@users.noreply.github.com>
2020-04-03 16:48:54 +0300

    Add files via upload

---
+++ slutsk_dynamic_02_0.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector <int> fib(n);
+    fib[0]=0;
+    fib[1]=1;
+    for(int i = 2; i <=n; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    cout << fib[n];
+    return 0;
+}
